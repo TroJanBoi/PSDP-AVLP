0be09d7dbbe61f5c9878e025b99cb8ea252d63dd
By_Pib <ddpatipan@gmail.com>
2025-02-23 17:28:55 +0700

    spilt asm leave main.go and crate route_asm func add, sub, mul, div, mov

--- Backend/main.go
+++ Backend/main.go
@@ -5,9 +5,6 @@ import (
 	"net/http"
 	"time"
 
-	"fmt"
-	"strings"
-
 	"example.com/greetings/database"
 	"example.com/greetings/models"
 	"example.com/greetings/routes"
@@ -26,65 +23,6 @@ func apiReadyHandler(c *gin.Context) {
 
 func htmlHandler(c *gin.Context) {
 	c.File("public/index.html")
-}
-
-type RequestData struct {
-	Instructions []string `json:"instructions"`
-}
-
-// Handler สำหรับประมวลผล Assembly Code
-func executeAssemblyHandler(c *gin.Context) {
-	var req RequestData
-	fmt.Println(req)
-	if err := c.BindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-	fmt.Println("Received Instructions:", req.Instructions)
-
-	// จำลอง registers เพื่อเก็บค่าการคำนวณ
-	registers := make(map[string]int)
-	for _, instr := range req.Instructions {
-		parts := strings.Fields(instr)
-		if len(parts) < 3 {
-			continue
-		}
-		fmt.Println("Processing Instruction:", instr)
-
-		switch parts[0] {
-		case "MOV":
-			// ตัวอย่าง: "MOV A, 5" จะทำการกำหนดค่าให้ A = 5
-			var value int
-			fmt.Sscanf(parts[2], "%d", &value)
-			registers[parts[1]] = value
-			fmt.Println("MOV:", parts[1], "=", value)
-		case "ADD":
-			// ตัวอย่าง: "ADD A, 10" จะทำการบวกค่า 10 ให้กับ A
-			currentValue := registers[parts[1]]
-			fmt.Println("Current Value in", parts[1], "before ADD:", currentValue)
-
-			num := 0
-			fmt.Sscanf(parts[2], "%d", &num)
-			registers[parts[1]] = currentValue + num
-			fmt.Println("ADD:", parts[1], "new value =", registers[parts[1]])
-		case "SUB":
-			// ตัวอย่าง: "ADD A, 10" จะทำการบวกค่า 10 ให้กับ A
-			currentValue := registers[parts[1]]
-			fmt.Println("Current Value in", parts[1], "before ADD:", currentValue)
-
-			num := 0
-			fmt.Sscanf(parts[2], "%d", &num)
-			registers[parts[1]] = currentValue - num
-			fmt.Println("ADD:", parts[1], "new value =", registers[parts[1]])
-		}
-	}
-	fmt.Println("Registers:", registers)
-
-	// ส่งผลลัพธ์กลับเป็น JSON โดยสมมุติว่า register A คือผลลัพธ์ที่ต้องการ
-	result := registers["A,"]
-	fmt.Println("Final Result (A):", result)
-	c.JSON(http.StatusOK, gin.H{"result": result})
-	// c.JSON(http.StatusOK, gin.H{"result": registers["A"]})
 }
 
 func main() {
@@ -108,10 +46,10 @@ func main() {
 	}))
 
 	routes.RegisterUserRoutes(r)
+	routes.RegisterAsmRoutes(r)
 
 	r.GET("/api-ready", apiReadyHandler)
 	r.GET("/", htmlHandler)
-	r.POST("/execute", executeAssemblyHandler)
 
 	r.Run(":9898")
 }
